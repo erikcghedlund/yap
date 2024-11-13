3d065c8839e829c8b40a3c5e20305758b95cc971
Erik Hedlund <erikcghedlund@outlook.com>
2024-11-13 20:29:45 +0000

    autoformat code with erlfmt (should be done automatically from now on

--- apps/yap/src/lexer.erl
+++ apps/yap/src/lexer.erl
@@ -8,48 +8,72 @@
 
 parse(Str) -> parse(Str, 1).
 
--spec parse(Str::[integer()], Line::integer()) -> [token].
-parse([$+|T], Line) -> [#token{type=binop, val=plussum, line=Line} | parse(T, Line)];
-parse([$-|T], Line) -> [#token{type=binop, val=minussym, line=Line} | parse(T, Line)];
-parse([$*|T], Line) -> [#token{type=binop, val=multsym, line=Line} | parse(T, Line)];
-parse([$/|T], Line) -> [#token{type=binop, val=slashsym, line=Line} | parse(T, Line)];
-parse([$<,$>|T], Line) -> [#token{type=relop, val=neqsym, line=Line} | parse(T, Line)];
-parse([$<,$=|T], Line) -> [#token{type=relop, val=leqsym, line=Line} | parse(T, Line)];
-parse([$>,$=|T], Line) -> [#token{type=relop, val=geqsym, line=Line} | parse(T, Line)];
-parse([$:,$=|T], Line) -> [#token{type=separator, val=becomesym, line=Line} | parse(T, Line)];
-parse([$<|T], Line) -> [#token{type=relop, val=lessym, line=Line} | parse(T, Line)];
-parse([$>|T], Line) -> [#token{type=relop, val=gtrsym, line=Line} | parse(T, Line)];
-parse([$=|T], Line) -> [#token{type=relop, val=equal, line=Line} | parse(T, Line)];
-parse([$,|T], Line) -> [#token{type=separator, val=commasym, line=Line} | parse(T, Line)];
-parse([$;|T], Line) -> [#token{type=separator, val=semicolonsym, line=Line} | parse(T, Line)];
-parse([$.|T], Line) -> [#token{type=separator, val=periodsym, line=Line} | parse(T, Line)];
-parse([$(|T], Line) -> [#token{type=separator, val=lparentsym, line=Line} | parse(T, Line)];
-parse([$)|T], Line) -> [#token{type=separator, val=rparentsym, line=Line} | parse(T, Line)];
-parse([$ |T], Line) -> parse(T, Line);
-parse([$\t|T], Line) -> parse(T, Line);
-parse([$\n|T], Line) -> parse(T, Line + 1);
-parse([C|T], Line) when ?IS_DIGIT(C) -> {Number, Tail} = lists:splitwith(fun(X) -> ?IS_DIGIT(X) end, [C|T]), {Value, _} = string:to_integer(Number), [#token{type=number, val=Value, line=Line} | parse(Tail, Line)];
-parse([C|T], Line) when ?IS_CHAR(C) -> {Ident, Tail} = lists:splitwith(fun(X) -> ?IS_DIGIT(X) or ?IS_CHAR(X) end, [C|T]),
+-spec parse(Str :: [integer()], Line :: integer()) -> [token].
+parse([$+ | T], Line) ->
+    [#token{type = binop, val = plussum, line = Line} | parse(T, Line)];
+parse([$- | T], Line) ->
+    [#token{type = binop, val = minussym, line = Line} | parse(T, Line)];
+parse([$* | T], Line) ->
+    [#token{type = binop, val = multsym, line = Line} | parse(T, Line)];
+parse([$/ | T], Line) ->
+    [#token{type = binop, val = slashsym, line = Line} | parse(T, Line)];
+parse([$<, $> | T], Line) ->
+    [#token{type = relop, val = neqsym, line = Line} | parse(T, Line)];
+parse([$<, $= | T], Line) ->
+    [#token{type = relop, val = leqsym, line = Line} | parse(T, Line)];
+parse([$>, $= | T], Line) ->
+    [#token{type = relop, val = geqsym, line = Line} | parse(T, Line)];
+parse([$:, $= | T], Line) ->
+    [#token{type = separator, val = becomesym, line = Line} | parse(T, Line)];
+parse([$< | T], Line) ->
+    [#token{type = relop, val = lessym, line = Line} | parse(T, Line)];
+parse([$> | T], Line) ->
+    [#token{type = relop, val = gtrsym, line = Line} | parse(T, Line)];
+parse([$= | T], Line) ->
+    [#token{type = relop, val = equal, line = Line} | parse(T, Line)];
+parse([$, | T], Line) ->
+    [#token{type = separator, val = commasym, line = Line} | parse(T, Line)];
+parse([$; | T], Line) ->
+    [#token{type = separator, val = semicolonsym, line = Line} | parse(T, Line)];
+parse([$. | T], Line) ->
+    [#token{type = separator, val = periodsym, line = Line} | parse(T, Line)];
+parse([$( | T], Line) ->
+    [#token{type = separator, val = lparentsym, line = Line} | parse(T, Line)];
+parse([$) | T], Line) ->
+    [#token{type = separator, val = rparentsym, line = Line} | parse(T, Line)];
+parse([$\s | T], Line) ->
+    parse(T, Line);
+parse([$\t | T], Line) ->
+    parse(T, Line);
+parse([$\n | T], Line) ->
+    parse(T, Line + 1);
+parse([C | T], Line) when ?IS_DIGIT(C) ->
+    {Number, Tail} = lists:splitwith(fun(X) -> ?IS_DIGIT(X) end, [C | T]),
+    {Value, _} = string:to_integer(Number),
+    [#token{type = number, val = Value, line = Line} | parse(Tail, Line)];
+parse([C | T], Line) when ?IS_CHAR(C) ->
+    {Ident, Tail} = lists:splitwith(fun(X) -> ?IS_DIGIT(X) or ?IS_CHAR(X) end, [C | T]),
     case Ident of
-        "call" -> [#token{type=keyword, val=callsym, line=Line} | parse(Tail, Line)];
-        "begin" -> [#token{type=keyword, val=beginsym, line=Line} | parse(Tail, Line)];
-        "read" -> [#token{type=keyword, val=readsym, line=Line} | parse(Tail, Line)];
-        "do" -> [#token{type=keyword, val=do, line=Line} | parse(Tail, Line)];
-        "while" -> [#token{type=keyword, val=whilesym, line=Line} | parse(Tail, Line)];
-        "write" -> [#token{type=keyword, val=writesym, line=Line} | parse(Tail, Line)];
-        "if" -> [#token{type=keyword, val=ifsym, line=Line} | parse(Tail, Line)];
-        "else" -> [#token{type=keyword, val=elsesym, line=Line} | parse(Tail, Line)];
-        "end" -> [#token{type=keyword, val=endsym, line=Line} | parse(Tail, Line)];
-        "odd" -> [#token{type=keyword, val=oddsym, line=Line} | parse(Tail, Line)];
-        "then" -> [#token{type=keyword, val=thensym, line=Line} | parse(Tail, Line)];
-        "const" -> [#token{type=keyword, val=constsym, line=Line} | parse(Tail, Line)];
-        "int" -> [#token{type=keyword, val=intsym, line=Line} | parse(Tail, Line)];
-        "procedure" -> [#token{type=keyword, val=procsym, line=Line} | parse(Tail, Line)];
-        "out" -> [#token{type=keyword, val=outsym, line=Line} | parse(Tail, Line)];
-        "in" -> [#token{type=keyword, val=insym, line=Line} | parse(Tail, Line)];
-        _ ->  [#token{type=ident, val=Ident, line=Line} | parse(Tail, Line)]
+        "call" -> [#token{type = keyword, val = callsym, line = Line} | parse(Tail, Line)];
+        "begin" -> [#token{type = keyword, val = beginsym, line = Line} | parse(Tail, Line)];
+        "read" -> [#token{type = keyword, val = readsym, line = Line} | parse(Tail, Line)];
+        "do" -> [#token{type = keyword, val = dosym, line = Line} | parse(Tail, Line)];
+        "while" -> [#token{type = keyword, val = whilesym, line = Line} | parse(Tail, Line)];
+        "write" -> [#token{type = keyword, val = writesym, line = Line} | parse(Tail, Line)];
+        "if" -> [#token{type = keyword, val = ifsym, line = Line} | parse(Tail, Line)];
+        "else" -> [#token{type = keyword, val = elsesym, line = Line} | parse(Tail, Line)];
+        "end" -> [#token{type = keyword, val = endsym, line = Line} | parse(Tail, Line)];
+        "odd" -> [#token{type = keyword, val = oddsym, line = Line} | parse(Tail, Line)];
+        "then" -> [#token{type = keyword, val = thensym, line = Line} | parse(Tail, Line)];
+        "const" -> [#token{type = keyword, val = constsym, line = Line} | parse(Tail, Line)];
+        "int" -> [#token{type = keyword, val = intsym, line = Line} | parse(Tail, Line)];
+        "procedure" -> [#token{type = keyword, val = procsym, line = Line} | parse(Tail, Line)];
+        "out" -> [#token{type = keyword, val = outsym, line = Line} | parse(Tail, Line)];
+        "in" -> [#token{type = keyword, val = insym, line = Line} | parse(Tail, Line)];
+        _ -> [#token{type = ident, val = Ident, line = Line} | parse(Tail, Line)]
     end;
-parse([], _) -> [].
+parse([], _) ->
+    [].
 
 parse_file(Filename) ->
     case file:read_file(Filename) of
@@ -58,6 +82,6 @@ parse_file(Filename) ->
     end.
 
 main(_Args) ->
-        Ans = lists:map(fun (File) -> parse_file(File) end, _Args),
-        lists:foreach(fun (X) -> lists:foreach(fun (E) ->  io:write(E) end, X) end, Ans),
-        halt().
+    Ans = lists:map(fun(File) -> parse_file(File) end, _Args),
+    lists:foreach(fun(X) -> io:write(X) end, Ans),
+    halt().
