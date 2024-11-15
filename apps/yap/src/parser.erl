4d67d4876cd1657203bf5d928ec8d2740cf824cb
Erik Hedlund <erikcghedlund@outlook.com>
2024-11-15 22:22:55 +0000

    implement ::= expression

--- apps/yap/src/parser.erl
+++ apps/yap/src/parser.erl
@@ -2,10 +2,30 @@
 -include_lib("eunit/include/eunit.hrl").
 -export([construct/1, construct/2]).
 
+construct(expression, [{token, Type, Val, Line}]) when (Type == number) or (Type == ident) ->
+    {expression, construct([{token, Type, Val, Line}])};
+construct(expression, [L, {token, binop, Sym, _}, R]) when (Sym == plussym) or (Sym == minussym) ->
+    Left = construct(term, [L]),
+    Right = construct(term, [R]),
+    Op =
+        case Sym of
+            plussym -> plus;
+            minussym -> minus
+        end,
+    {expression, Left, Op, Right};
+construct(expression, [L, {token, binop, Sym, _} | R]) when (Sym == plussym) or (Sym == minussym) ->
+    Left = construct(term, [L]),
+    Right = construct(expression, R),
+    Op =
+        case Sym of
+            plussym -> plus;
+            minussym -> minus
+        end,
+    {expression, Left, Op, Right};
 construct(term, [{token, Type, Val, Line}]) when (Type == number) or (Type == ident) ->
     {term, construct(factor, [{token, Type, Val, Line}])};
 % TODO: This case might be redundant (or not), hmm...
-construct(term, [L, {token, binop, Sym, _}, R]) when (Sym == multsym) or (Sym == slashsym) ->
+construct(term, [L, {token, binop, Sym, _}, R]) ->
     Left = construct(factor, [L]),
     Right = construct(factor, [R]),
     case {Sym, Left, Right} of
--- apps/yap/src/parser_tests.erl
+++ apps/yap/src/parser_tests.erl
@@ -66,3 +66,36 @@ construct_term10_test() ->
                 {term, {factor, number, 3}, ddiv, {factor, ident, "two"}}}},
         parser:construct(term, lexer:parse("one / res * 3 / two"))
     ).
+
+construct_expression1_test() ->
+    ?assertEqual(
+        {expression, {term, {factor, ident, "one"}}},
+        parser:construct(expression, lexer:parse("one"))
+    ).
+construct_expression2_test() ->
+    ?assertEqual(
+        {expression, {term, {factor, number, 1}}},
+        parser:construct(expression, lexer:parse("1"))
+    ).
+construct_expression3_test() ->
+    ?assertEqual(
+        {expression, {term, {factor, ident, "one"}}, plus, {term, {factor, ident, "two"}}},
+        parser:construct(expression, lexer:parse("one + two"))
+    ).
+construct_expression4_test() ->
+    ?assertEqual(
+        {expression, {term, {factor, number, 1}}, minus, {term, {factor, number, 3}}},
+        parser:construct(expression, lexer:parse("1 - 3"))
+    ).
+construct_expression5_test() ->
+    ?assertEqual(
+        {expression, {term, {factor, ident, "one"}}, plus,
+            {expression, {term, {factor, ident, "two"}}, minus, {term, {factor, ident, "three"}}}},
+        parser:construct(expression, lexer:parse("one + two - three"))
+    ).
+construct_expression6_test() ->
+    ?assertEqual(
+        {expression, {term, {factor, number, 1}}, minus,
+            {expression, {term, {factor, number, 3}}, plus, {term, {factor, number, 5}}}},
+        parser:construct(expression, lexer:parse("1 - 3 + 5"))
+    ).
