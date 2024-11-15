d15ca82281b7149df01de3609b788f0a098458f4
Erik Hedlund <erikcghedlund@outlook.com>
2024-11-15 15:49:25 +0000

    implement (term ::=) except paranthesises)

--- apps/yap/src/parser.erl
+++ apps/yap/src/parser.erl
@@ -1,27 +1,33 @@
 -module(parser).
 -include_lib("eunit/include/eunit.hrl").
--export([construct/1]).
--record(tree, {left :: any(), op :: any(), right :: any()}).
-% -record(ident, {val :: binary:unicode()}).
-% -record(number, {val :: binary:unicode()}).
-% -record(expression, {type :: atom(), val :: any(), line :: integer()}).
+-export([construct/1, construct/2]).
 
-construct([L, {token, binop, Sym, _}, R]) when (Sym == multsym) or (Sym == slashsym) ->
-    Left = construct(L),
-    Right = construct(R),
-    io:write(Left),
-    io:write(Right),
+construct(term, [{token, Type, Val, Line}]) when (Type == number) or (Type == ident) ->
+    {term, construct(factor, [{token, Type, Val, Line}])};
+construct(term, [L, {token, binop, Sym, _}, R]) when (Sym == multsym) or (Sym == slashsym) ->
+    Left = construct(factor, [L]),
+    Right = construct(factor, [R]),
     case {Sym, Left, Right} of
         {multsym, {factor, _, _}, {factor, _, _}} ->
-            #tree{left = Left, op = mul, right = Right};
+            {term, Left, mul, Right};
         {slashsym, {factor, _, _}, {factor, _, _}} ->
-            #tree{left = Left, op = ddiv, right = Right};
+            {term, Left, ddiv, Right};
         _ ->
             error("Illegal syntax")
     end;
-construct({token, ident, Val, _}) ->
+construct(term, [L, {token, binop, Sym, _} | R]) when (Sym == multsym) or (Sym == slashsym) ->
+    Left = construct(factor, [L]),
+    Right = construct(term, R),
+    case {Sym, Left, Right} of
+        {multsym, {factor, _, _}, {term, _, _, _}} ->
+            {term, Left, mul, Right};
+        {slashsym, {factor, _, _}, {term, _, _, _}} ->
+            {term, Left, ddiv, Right};
+        _ ->
+            error("Illegal syntax")
+    end;
+construct(factor, [{token, ident, Val, _}]) ->
     {factor, ident, Val};
-construct({token, number, Val, _}) ->
-    {factor, number, Val};
-construct([]) ->
-    [].
+construct(factor, [{token, number, Val, _}]) ->
+    {factor, number, Val}.
+construct(Tokens) -> construct(term, Tokens).
--- apps/yap/src/parser_tests.erl
+++ apps/yap/src/parser_tests.erl
@@ -1,33 +1,68 @@
 -module(parser_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+construct_factor1_test() ->
+    ?assertEqual(
+        {factor, ident, "one"},
+        parser:construct(factor, lexer:parse("one"))
+    ).
+construct_factor2_test() ->
+    ?assertEqual(
+        {factor, number, 1},
+        parser:construct(factor, lexer:parse("1"))
+    ).
+
+construct_term7_test() ->
+    ?assertEqual(
+        {term, {factor, ident, "one"}},
+        parser:construct(term, lexer:parse("one"))
+    ).
+construct_term8_test() ->
+    ?assertEqual(
+        {term, {factor, number, 1}},
+        parser:construct(term, lexer:parse("1"))
+    ).
 construct_term1_test() ->
     ?assertEqual(
-        {tree, {factor, number, 1}, mul, {factor, number, 2}},
-        parser:construct(lexer:parse("1 * 2"))
+        {term, {factor, number, 1}, mul, {factor, number, 2}},
+        parser:construct(term, lexer:parse("1 * 2"))
     ).
 construct_term2_test() ->
     ?assertEqual(
-        {tree, {factor, number, 9}, ddiv, {factor, number, 2}},
-        parser:construct(lexer:parse("9 / 2"))
+        {term, {factor, number, 9}, ddiv, {factor, number, 2}},
+        parser:construct(term, lexer:parse("9 / 2"))
     ).
 construct_term3_test() ->
     ?assertEqual(
-        {tree, {factor, ident, "x"}, mul, {factor, number, 2}},
-        parser:construct(lexer:parse("x * 2"))
+        {term, {factor, ident, "x"}, mul, {factor, number, 2}},
+        parser:construct(term, lexer:parse("x * 2"))
     ).
 construct_term4_test() ->
     ?assertEqual(
-        {tree, {factor, number, 1}, ddiv, {factor, ident, "res"}},
-        parser:construct(lexer:parse("1 / res"))
+        {term, {factor, number, 1}, ddiv, {factor, ident, "res"}},
+        parser:construct(term, lexer:parse("1 / res"))
     ).
 construct_term5_test() ->
     ?assertEqual(
-        {tree, {factor, ident, "a"}, mul, {factor, ident, "b"}},
-        parser:construct(lexer:parse("a * b"))
+        {term, {factor, ident, "a"}, mul, {factor, ident, "b"}},
+        parser:construct(term, lexer:parse("a * b"))
     ).
 construct_term6_test() ->
     ?assertEqual(
-        {tree, {factor, ident, "one"}, ddiv, {factor, ident, "res"}},
-        parser:construct(lexer:parse("one / res"))
+        {term, {factor, ident, "one"}, ddiv, {factor, ident, "res"}},
+        parser:construct(term, lexer:parse("one / res"))
+    ).
+construct_term9_test() ->
+    ?assertEqual(
+        {term, {factor, ident, "a"}, mul,
+            {term, {factor, ident, "b"}, mul,
+                {term, {factor, ident, "c"}, mul, {factor, ident, "d"}}}},
+        parser:construct(term, lexer:parse("a * b * c * d"))
+    ).
+construct_term10_test() ->
+    ?assertEqual(
+        {term, {factor, ident, "one"}, ddiv,
+            {term, {factor, ident, "res"}, mul,
+                {term, {factor, number, 3}, ddiv, {factor, ident, "two"}}}},
+        parser:construct(term, lexer:parse("one / res * 3 / two"))
     ).
