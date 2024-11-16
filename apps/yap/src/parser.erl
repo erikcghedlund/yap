6c6dab512823e57f6925a0ef9622d882281a1abd
Erik Hedlund <erikcghedlund@outlook.com>
2024-11-16 18:14:22 +0000

    Implement condition ::=

--- apps/yap/src/parser.erl
+++ apps/yap/src/parser.erl
@@ -2,8 +2,13 @@
 -include_lib("eunit/include/eunit.hrl").
 -export([construct/1, construct/2]).
 
+construct(condition, [{token, keyword, oddsym, _} | Tokens]) ->
+    {condition, {odd, construct(expression, Tokens)}};
+construct(condition, Tokens) ->
+    {Before, Symbol, After} = find_symbol(type, [relop], Tokens),
+    {condition, {construct(expression, Before), Symbol, construct(expression, After)}};
 construct(expression, Tokens) ->
-    case find_symbol([plussym, minussym], Tokens) of
+    case find_symbol(symbol, [plussym, minussym], Tokens) of
         {Before, undefined, []} ->
             {expression, construct(term, Before)};
         {Before, Symbol, After} ->
@@ -11,7 +16,7 @@ construct(expression, Tokens) ->
                 construct(expression, After)}
     end;
 construct(term, Tokens) ->
-    case find_symbol([multsym, slashsym], Tokens) of
+    case find_symbol(symbol, [multsym, slashsym], Tokens) of
         {Before, undefined, []} ->
             {term, construct(factor, Before)};
         {Before, Symbol, After} ->
@@ -21,14 +26,20 @@ construct(factor, [{token, Type, Val, _}]) when (Type == ident) or (Type == numb
     {factor, Type, Val}.
 construct(Tokens) -> construct(term, Tokens).
 
-find_symbol(Symbols, Tokens) -> find_symbol(Symbols, Tokens, []).
-find_symbol(_, [], Passed) ->
+find_symbol(Field, Symbols, Tokens) -> find_symbol(Field, Symbols, Tokens, []).
+find_symbol(_, _, [], Passed) ->
     {lists:reverse(Passed), undefined, []};
-find_symbol(Symbols, [{token, Type, TSym, Line} | Tokens], Passed) ->
+find_symbol(symbol, Symbols, [{token, Type, TSym, Line} | Tokens], Passed) ->
     Is_member = lists:member(TSym, Symbols),
     if
         Is_member -> {lists:reverse(Passed), TSym, Tokens};
-        true -> find_symbol(Symbols, Tokens, [{token, Type, TSym, Line} | Passed])
+        true -> find_symbol(symbol, Symbols, Tokens, [{token, Type, TSym, Line} | Passed])
+    end;
+find_symbol(type, Types, [{token, Type, TSym, Line} | Tokens], Passed) ->
+    Is_member = lists:member(Type, Types),
+    if
+        Is_member -> {lists:reverse(Passed), TSym, Tokens};
+        true -> find_symbol(symbol, Types, Tokens, [{token, Type, TSym, Line} | Passed])
     end.
 
 symbol_to_op(multsym) -> mul;
